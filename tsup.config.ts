d414f42d2a45679e24187d29f4674c0090830c26
Yehuda Ringler <yrappdev@gmail.com>
2026-06-03 09:24:08 -0400

    remove export json, toggle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from "tsup";
-import { SEDARIM } from "./src/tractates.js";
 
-const jsonExternals = SEDARIM.flatMap((s) =>
-  s.tractates.map((t) => `../data/${t.key}.json`)
-);
-
-export default defineConfig([{
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  clean: true,
-  splitting: false,
-  external: jsonExternals,
-}, {
-  entry: ["src/tractate-index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  splitting: false,
-}]);
+export default defineConfig([
+    {
+        entry: ["src/tractate-index.ts"],
+        format: ["esm", "cjs"],
+        dts: true,
+        splitting: false,
+    },
+]);
