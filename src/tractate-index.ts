cf64261907211e3494b0014fbf278dc20dfe4bbb
Yehuda Ringler <yrappdev@gmail.com>
2026-06-03 09:31:26 -0400

    add simple cache

--- src/tractate-index.ts
+++ src/tractate-index.ts
@@ -2,6 +2,7 @@ import index from "../data/index.json" assert { type: "json" };
 import type { Tractate } from "./types.js";
 
 const tractateIndex: Record<string, string> = index;
+const tractateCache: Record<string, Tractate> = {};
 
 export function getJsonFileName(englishName: string): string | undefined {
   return tractateIndex[englishName];
@@ -12,6 +13,10 @@ export async function getTractate(base: string, englishName: string): Promise<Tr
   if (!fileName) {
     throw new Error(`Tractate not found: ${englishName}`);
   }
+  if (tractateCache[englishName]) {
+    return tractateCache[englishName];
+  }
   const { default: tractate } = await import(`${base}/${fileName}`, { assert: { type: "json" } });
-  return tractate as Tractate;
+  tractateCache[englishName] = tractate as Tractate;
+  return tractateCache[englishName];
 }
